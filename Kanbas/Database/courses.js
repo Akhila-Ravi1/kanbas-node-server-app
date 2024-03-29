db9d147d9ba13f23b715f3a54cc121f22a546b37
Akhila-Ravi1 <akhilar028@gmail.com>
2024-03-29 16:09:40 -0400

    Kanbas - 4.1 and 4.2 done

---
+++ Kanbas/Database/courses.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        "_id": "RS101",
+        "name": "React JS",
+        "number": "CS1234",
+        "semester": "202410_1 Spring 2023 Semester",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "blue.png"
+    },
+    {
+        "_id": "RS102",
+        "name": "Machine Learning",
+        "number": "CS6140",
+        "semester": "202410_1 Spring 2023 Semester",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "brown.png"
+    },
+    {
+        "_id": "RS103",
+        "name": "Data Mining",
+        "number": "CS6100",
+        "semester": "202410_1 Spring 2023 Semester",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "green.png"
+    },
+    {
+        "_id": "RS104",
+        "name": "Data Visualization",
+        "number": "CS6101",
+        "semester": "202410_1 Spring 2023 Semester",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "orange.png"
+    },
+    {
+        "_id": "RS105",
+        "name": "Big Data Analytics",
+        "number": "CS5140",
+        "semester": "202410_1 Spring 2023 Semester",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "lilac.png"
+    },
+    {
+        "_id": "RS106",
+        "name": "Cloud Computing",
+        "number": "CS5100",
+        "semester": "202410_1 Spring 2023 Semester",
+        "startDate": "2023-01-10",
+        "endDate": "2023-05-15",
+        "image": "maroon.png"
+    }
+]
