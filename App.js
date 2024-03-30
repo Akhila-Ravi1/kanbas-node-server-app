ba9539881321797b951439bb17c8b42f436ed9e9
Akhila-Ravi1 <akhilar028@gmail.com>
2024-03-30 15:33:58 -0400

    Kanbas - 4.3.5 done

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import Lab5 from './Lab5.js';
 import cors from 'cors';
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from './Kanbas/modules/routes.js';
+import AssignmentRoutes from './Kanbas/assignments/routes.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
+AssignmentRoutes(app);
 
 Lab5(app);
 Hello(app);
