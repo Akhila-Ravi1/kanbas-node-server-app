f6bf7d7057a62edddf4edf9e6995b02e13484244
Akhila-Ravi1 <akhilar028@gmail.com>
2024-04-14 19:09:23 -0400

    code - part 3 done

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
+import UserRoutes from './Users/routes.js';
 import cors from 'cors';
 import CourseRoutes from './Kanbas/courses/routes.js';
 import ModuleRoutes from './Kanbas/modules/routes.js';
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
 import QuizRoutes from './Kanbas/quizzes/routes.js';
+import mongoose from 'mongoose';
 
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+UserRoutes(app);
 
 ModuleRoutes(app);
 CourseRoutes(app);
