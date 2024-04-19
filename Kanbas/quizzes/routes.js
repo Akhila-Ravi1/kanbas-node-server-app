2882c274379563ae809822d1000944576f5672a5
Akhila-Ravi1 <akhilar028@gmail.com>
2024-04-18 21:39:31 -0400

    Migrated quiz to MongoDB

--- Kanbas/quizzes/routes.js
+++ Kanbas/quizzes/routes.js
@@ -1,38 +1,44 @@
-import db from "../Database/index.js";
+import * as dao from "./dao.js";
 
-function QuizRoutes(app) {
-    app.get("/api/courses/:cid/quizzes", (req, res) => {
-        const { cid } = req.params;
-        const quizzes = db.quizzes.filter((a) => a.course === cid);
-        res.send(quizzes);
-    });
+export default function QuizRoutes(app) {
+    const deleteQuiz = async (req, res) => {
+        const status = await dao.deleteQuiz(req.params.id);
+        res.json(status);
+    };
 
-    app.post("/api/courses/:cid/quizzes", (req, res) => {
-        const { cid } = req.params;
-        const newQuiz = {
-            ...req.body,
-            course: cid,
-            _id: new Date().getTime().toString()
-        };
-        db.quizzes.push(newQuiz);
-        res.send(newQuiz);
-    });
+    const updateQuiz = async (req, res) => {
+        const { id } = req.params;
+        const status = await dao.updateQuiz(id, req.body);
+        res.sendStatus(204);
+    };
 
-    app.put("/api/quizzes/:qid", (req, res) => {
-        const { qid } = req.params;
-        const quizIndex = db.quizzes.findIndex((q) => q._id === qid);
-        db.quizzes[quizIndex] = {
-            ...db.quizzes[quizIndex],
-            ...req.body
-        };
-        res.send(db.quizzes[quizIndex]);
-    });
+    const getAllQuizzes = async (req, res) => {
+        const quizzes = await dao.findAllQuizzes();
+        res.json(quizzes);
+    };
 
-    app.delete("/api/quizzes/:qid", (req, res) => {
-        const { qid } = req.params;
-        db.quizzes = db.quizzes.filter((q) => q._id !== qid);
-        res.sendStatus(200);
-    });
+    const getQuizById = async (req, res) => {
+        const quiz = await dao.findQuizById(req.params.id);
+        if (!quiz) {
+            res.status(404).send("Quiz not found");
+            return;
+        }
+        res.json(quiz);
+    }
+
+    const createQuiz = async (req, res) => {
+        try {
+            const quiz = await dao.createQuiz(req.body);
+            res.json(quiz);
+        } catch (error) {
+            res.status(404).send("Quiz not created! Try again!");
+            return;
+        }
+    }
+
+    app.get("/api/courses/:cid/quizzes", getAllQuizzes);
+    app.get("/api/courses/:cid/quizzes/:id", getQuizById);
+    app.delete("/api/quizzes/:id", deleteQuiz);
+    app.put("/api/quizzes/:id", updateQuiz);
+    app.post("/api/courses/:cid/quizzes", createQuiz);
 }
-
-export default QuizRoutes;
